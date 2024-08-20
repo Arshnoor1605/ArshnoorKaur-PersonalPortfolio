b064bf71b84ef598cd234e292a20480d66549281
Arshnoor Kaur <arshnoor1605@gmail.com>
2024-08-20 13:58:54 -0400

    exp

--- components/spotlightName.tsx
+++ components/spotlightName.tsx
@@ -45,7 +45,40 @@ export function SpotlightPreview() {
         <h1 className="m-10 text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-cyan-300 to-blue-400 bg-opacity-50">
           About Me
         </h1>
-        <Timeline />
+        <p className="text-white">
+          Excepteur nostrud sunt amet sint magna. Tempor minim eu tempor sint
+          occaecat fugiat commodo. Nisi nulla proident ad eiusmod mollit.
+          Consectetur ullamco adipisicing sint sint ullamco irure sit. Laborum
+          qui voluptate in magna commodo ut et consequat qui quis consequat
+          sint. Ullamco ad laboris qui nulla in adipisicing officia non
+          incididunt in sint. Aliquip commodo laborum eu laboris Lorem et culpa
+          amet amet nostrud laborum culpa adipisicing est. Esse incididunt
+          adipisicing et qui nisi ullamco. Pariatur laborum mollit et amet sint
+          officia nostrud cillum irure cupidatat ullamco. Nulla ex quis
+          reprehenderit ut ad culpa. Et tempor consectetur incididunt anim
+          consequat consectetur deserunt commodo. Amet ipsum fugiat enim quis
+          sunt occaecat dolor nulla labore sunt amet cillum esse commodo.
+          Reprehenderit proident nostrud ea deserunt id tempor do dolore. Labore
+          id ex Lorem aliqua consectetur nulla id dolore cillum reprehenderit
+          aliquip.Enim pariatur dolor adipisicing anim ex ipsum commodo. Sit
+          commodo dolor eiusmod voluptate sit mollit Lorem reprehenderit minim
+          anim laborum. Consequat anim sit nulla nostrud consectetur laboris
+          amet. Ad elit culpa pariatur nisi irure officia in consequat quis.
+          Dolor laboris irure velit aliquip aliquip consectetur consectetur qui
+          voluptate excepteur adipisicing cillum excepteur. Aute id irure
+          deserunt ut sunt nulla. Amet qui velit magna ad nisi non
+          mollit.Excepteur nostrud minim velit esse non ex nulla labore proident
+          cupidatat. Enim ad commodo culpa amet nisi commodo amet. Velit esse
+          ipsum Lorem est esse amet ut voluptate mollit. Amet reprehenderit
+          laborum et cupidatat voluptate consequat culpa et irure veniam
+          occaecat. Irure sit reprehenderit ut fugiat incididunt velit anim et
+          incididunt eu veniam laboris.Adipisicing ex irure voluptate pariatur
+          velit est culpa excepteur anim. Adipisicing qui aliqua esse cillum
+          Lorem qui dolor minim anim commodo tempor officia. Id sit nisi do amet
+          elit nisi ea est amet. Reprehenderit qui in nisi do voluptate esse
+          culpa in aute ea irure amet. Lorem proident sit culpa ullamco in
+          nostrud eu ullamco ut cillum minim.
+        </p>
       </div>
     </div>
   );
