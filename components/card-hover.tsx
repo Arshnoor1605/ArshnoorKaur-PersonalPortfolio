944e22c98d538a62588bb90bb0924f022c1cc855
Arshnoor Kaur <arshnoor1605@gmail.com>
2024-11-11 21:06:03 -0500

    Made changes to the container grid

--- components/card-hover.tsx
+++ components/card-hover.tsx
@@ -9,37 +9,37 @@ export function CardHoverEffectDemo() {
 }
 export const projects = [
   {
-    title: "Stripe",
+    title: "Operating Systems",
     description:
       "A technology company that builds economic infrastructure for the internet.",
     link: "https://stripe.com",
   },
   {
-    title: "Netflix",
+    title: "Data Structures and Algorithms",
     description:
       "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
     link: "https://netflix.com",
   },
   {
-    title: "Google",
+    title: "Computer Architecture",
     description:
       "A multinational technology company that specializes in Internet-related services and products.",
     link: "https://google.com",
   },
   {
-    title: "Meta",
+    title: "Human Computer Interfaces",
     description:
       "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
     link: "https://meta.com",
   },
   {
-    title: "Amazon",
+    title: "Software Development",
     description:
       "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
     link: "https://amazon.com",
   },
   {
-    title: "Microsoft",
+    title: "Statistics and Probability",
     description:
       "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
     link: "https://microsoft.com",
