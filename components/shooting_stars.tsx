9d210ff66cd7a610260c1f221f43f92182c53c36
Arshnoor Kaur <arshnoor1605@gmail.com>
2024-12-21 03:51:41 +0530

    Added border to my pic and reduced the size of my pic

--- components/shooting_stars.tsx
+++ components/shooting_stars.tsx
@@ -3,18 +3,20 @@ import React from "react";
 import { ShootingStars } from "./shooting-stars";
 import { StarsBackground } from "./stars-background";
 import { HeroScrollDemo } from "./container_scroll";
+import Image from "next/image";
+
 export function ShootingStarsAndStarsBackgroundDemo() {
   return (
     <div className="h-full rounded-md bg-black flex flex-col items-center justify-center relative w-full">
       <HeroScrollDemo />
       <ShootingStars />
       <StarsBackground />
-      <img
+      <Image
         src="/headshot.jpeg"
         alt="my headshot"
         width={270}
         height={220}
-        className="object-cover border-emerald-100 border-2 md:w-48 md:h-48"
+        className="object-cover border-emerald-100 border-2 md:w-48 md:h-48 max-w-full"
       />
     </div>
   );
