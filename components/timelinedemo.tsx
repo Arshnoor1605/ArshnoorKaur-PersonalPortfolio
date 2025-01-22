9ceee61dfb3e712ad8d5c4941bc1e514b09d88f9
Arshnoor Kaur <arshnoor1605@gmail.com>
2025-01-22 16:14:00 -0500

    Removed the extra projects from my timeline

--- components/timelinedemo.tsx
+++ components/timelinedemo.tsx
@@ -132,58 +132,6 @@ export function TimelineDemo() {
         </div>
       ),
     },
-    {
-      title: "2023 and before",
-      content: (
-        <div>
-          <p className="text-teal-200 dark:text-neutral-200 text-lg md:text-3xl font-bold mb-8">
-            Mini-projects using functional programming and SQL + python
-          </p>
-          <div className="mb-8">
-            <div className="flex gap-2 items-center text-neutral-100 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Among Us
-            </div>
-            <div className="flex gap-2 items-center text-neutral-100 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Math Visualizer
-            </div>
-            <div className="flex gap-2 items-center text-neutral-100 dark:text-neutral-300 text-xs md:text-sm">
-              ✅ Restaurant Management system using relational databases (SQL +
-              Python)
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-            <Image
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
-              width={500}
-              height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            />
-          </div>
-        </div>
-      ),
-    },
   ];
   return (
     <div className="w-full">
